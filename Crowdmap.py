59b985d649cb8290c506b62f489d936b38b6a300
zoya-sh <shaulove.zoya@gmail.com>
2015-05-20 17:54:53 +0300

    contnie

--- Crowdmap.py
+++ Crowdmap.py
@@ -1,6 +1,20 @@
-class Crowdmap (object):
-	def _init_(self,init_list):
-	self.list = init_list
+class Crowdmap(object):
+	def __init__(self, init_list):
+		self.list = init_list
 	
-	def get_all_posts_for(self,param):
-		return [post for post in self.list if.find(param) !=-1]
+	def get_all_posts_for(self, name):
+		return [post for post in self.list if post.find(name) != -1]
+		
+	def is_location_for_name(self, name):
+		posts = [post for post in self.list if post.find(name) != -1 and post.find('at') != -1]
+		return len(posts) != 0
+		
+	def is_map_consistence(self, name):
+		existsInMap = False
+		for i in self.list:
+			if i.find(name) != -1 and existsInMap:
+				return False
+			if i.find(name) != -1:
+				existInMap = True
+		
+		return True 
